f4d95f67cfbbfad923f542d3ec745b14d6e6070d
Michael Franke <the.secret.goldfish@me.com>
2021-12-19 20:44:31 +0100

    updated trial ordering

--- src/App.vue
+++ src/App.vue
@@ -25,14 +25,25 @@
 
       <Slide>
         <Record :data="{
-                       trialNr :i+1,
-                       StimID : item.StimID,
-                       Trialtype : item.TrialType,
-                       ContextType : item.ContextType,
+                       trialNr         : i+1,
+                       StimID          : item.StimID,
+                       TrialType       : item.TrialType,
+                       TaskType        : item.TaskType,
+                       ContextType     : item.ContextType,
                        AnswerCertainty : item.AnswerCertainty,
-                       AnswerPolarity : item.AnswerPolarity,
+                       AnswerPolarity  : item.AnswerPolarity,
+                       p_min           : item.p_min,
+                       p_max           : item.p_max,
+                       certainty_min   : item.certainty_min,
+                       certainty_max   : item.certainty_max,
                        }"
                 />
+        <KeypressInput
+          :response.sync= "$magpie.measurements.lunch"
+          :keys="{
+                 '~': 'next'
+                 }"
+          @update:response="$magpie.saveAndNextScreen();" />
         <span style="color:gray">Context:</span> {{item.Context}}
         <br>
         <br>
@@ -41,13 +52,13 @@
         <strong>"{{item.YourQuestion}}"</strong>
         <br>
         <br>
-        <div  v-if="! (item.TrialType.includes('prior') | item.StimID == 'reasoning_1' | item.StimID == 'reasoning_2')"
+        <div  v-if="! (item.TaskType.includes('prior') | item.TrialType.includes('reasoning'))"
               style="background-color:lightblue; display:inline-block">
           {{item.AnswerIntro}}
           <br>
           <strong>"{{item.Answer}}"</strong>
         </div>
-        <div  v-if="! (item.TrialType.includes('prior')) && (item.StimID == 'reasoning_1' | item.StimID == 'reasoning_2') "
+        <div  v-if="! (item.TaskType.includes('prior')) && item.TrialType.includes('reasoning')"
               style="background-color:lightblue; display:inline-block">
           <strong>{{item.Answer}}</strong>
         </div>
@@ -62,14 +73,14 @@
           :disabled="sliderResponseClicked=='true'"
           :initial="0"/>
         <span v-if="$magpie.measurements.sliderResponse >=0
-                    && ! item.TrialType.includes('relevance')"
+                    && ! item.TaskType.includes('relevance')"
               style="color:gray">
           Your selection means that there is a
           {{$magpie.measurements.sliderResponse}}% chance that
           {{item.CriticalProposition}}.
         </span>
         <span v-if="$magpie.measurements.sliderResponse >=0
-                    && item.TrialType.includes('relevance')"
+                    && item.TaskType.includes('relevance')"
               style="color:gray">
           Your selection means that you give this answer a helpfulness score of
           {{$magpie.measurements.sliderResponse}} on a scale from 0 to 100.
@@ -77,28 +88,28 @@
 
         <button v-if="($magpie.measurements.sliderResponse >=0
                       && sliderResponseClicked =='false'
-                      && ! item.TrialType.includes('relevance'))"
+                      && ! item.TaskType.includes('relevance'))"
                 @click="toggleSliderResponseFlag()">
           Continue
         </button>
         <br>
         <strong v-if="sliderResponseClicked=='true'
-                      && ! item.TrialType.includes('relevance')">
+                      && ! item.TaskType.includes('relevance')">
           How confident are you that the probability is
           {{$magpie.measurements.sliderResponse}}%?
         </strong>
         <RatingInput
-          v-if="sliderResponseClicked=='true' && ! item.TrialType.includes('relevance')"
+          v-if="sliderResponseClicked=='true' && ! item.TaskType.includes('relevance')"
           left="highly unsure"
           right="highly confident"
           :response.sync= "$magpie.measurements.confidence"
           />
         <button v-if="$magpie.measurements.confidence >=0
-                      && ! item.TrialType.includes('relevance')"
+                      && ! item.TaskType.includes('relevance')"
                 @click="toggleSliderResponseFlag();$magpie.saveAndNextScreen();">
           Submit
         </button>
-        <button v-if="$magpie.measurements.sliderResponse >=0 && item.TrialType.includes('relevance')"
+        <button v-if="$magpie.measurements.sliderResponse >=0 && item.TaskType.includes('relevance')"
                 @click="$magpie.saveAndNextScreen();">
           Submit
         </button>
@@ -119,8 +130,10 @@
 // Load data from csv files as javascript arrays with objects
 import relevanceItems from '../trials/relevance_stimuli.csv';
 import fillerItems from '../trials/relevance_fillers.csv';
-import answerConditions from '../trials/answer-conditions.csv';
+import answerConditionsRaw from '../trials/answer-conditions.csv';
 import _ from 'lodash';
+
+var answerConditions = _.shuffle(answerConditionsRaw);
 
 // creating trial structure
 var vignettes = _.slice(_.shuffle(_.range(1,12)), 0, 10)
@@ -137,12 +150,12 @@ var mainItems = _.flatMap(_.range(0,10), function(i) {
 // console.log(mainItems)
 
 var items =
-    _.slice(mainItems,0,2).concat(
+    _.slice(mainItems,0,3).concat(
         fillerItems[0],
         fillerItems[1],
-        // _.slice(mainItems,6,12),
+        _.slice(mainItems,6,12),
         fillerItems[4],
-        // _.slice(mainItems,12,18),
+        _.slice(mainItems,12,18),
         fillerItems[2],
         fillerItems[3],
         _.slice(mainItems,18,24),
