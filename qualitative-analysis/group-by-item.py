8244c7c2d29bb608f7da8df203c06dc3655ae1a0
osagha <omarsagha@LAPTOP-1D1CTLDB.localdomain>
2023-11-19 09:20:45 -0500

    begin qualitative analysis

---
+++ qualitative-analysis/group-by-item.py
@@ -0,0 +1,149 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+relevance_dir = Path(__file__).resolve().parent.parent
+# INPUT
+data_with_exclusions_path = relevance_dir / 'qualitative-analysis' / 'data' / 'included_data.csv'
+
+# OUTPUT
+data_by_item_path = relevance_dir / 'qualitative-analysis' / 'data' / 'by_item.csv'
+
+# Read the round 2 data with exclusions applied.
+d = pd.read_csv(data_with_exclusions_path)
+
+# Drop column `group` (`relevant` vs `helpful`)
+# since we found it makes no difference.
+# Extract the columns that are relevant for first-order measures only. Begin by partitioning all columns into four categories.
+
+dropped_cols = [
+    'submission_id',
+    'attention_score',
+    'reasoning_score',
+    'group',
+]
+
+group_by_cols = [
+    'StimID',
+    'AnswerCertainty',
+    'AnswerPolarity',
+    'ContextType',
+]
+
+first_order_cols = [
+    'posterior_sliderResponse',
+    'prior_sliderResponse',
+    'relevance_sliderResponse',
+    'first_order_belief_change',
+    'entropy_change',
+    'kl_utility',
+    'bayes_factor_utility',
+]
+
+second_order_cols = [
+    'posterior_confidence',
+    'prior_confidence', 
+    'second_order_belief_change',
+    'beta_entropy_change',
+    'beta_kl_utility',
+    'beta_bayes_factor_utility',
+    'pure_second_order_belief_change',
+    'prior_beta_for_kl_a',
+    'prior_beta_for_kl_b',
+    'posterior_beta_for_kl_a',
+    'posterior_beta_for_kl_b',
+    'prior_beta_for_entropy_a',
+    'prior_beta_for_entropy_b',
+    'posterior_beta_for_entropy_a',
+    'posterior_beta_for_entropy_b',
+    'prior_beta_for_bf_a',
+    'prior_beta_for_bf_b',
+    'posterior_beta_for_bf_a',
+    'posterior_beta_for_bf_b',
+    'prior_beta_for_2nd_order_change_a',
+    'prior_beta_for_2nd_order_change_b',
+    'posterior_beta_for_2nd_order_change_a',
+    'posterior_beta_for_2nd_order_change_b',
+]
+
+# Check that the partition is exhaustive.
+assert(set(d.columns) == set(dropped_cols + group_by_cols + first_order_cols + second_order_cols))
+
+# Rename columns of interest (all first order).
+first_order_col_renames = {
+    'posterior_sliderResponse'  : 'pos',
+    'prior_sliderResponse'      : 'pri',
+    'relevance_sliderResponse'  : 'rel',
+    'first_order_belief_change' : 'bch',
+    'entropy_change'            : 'ech',
+    'kl_utility'                : 'klu',
+    'bayes_factor_utility'      : 'bfu',
+}
+
+# Treat categorical columns as categorical.
+d = d.astype(
+    { 
+    'StimID': 'category',
+    'AnswerCertainty': 'category',
+    'AnswerPolarity': 'category',
+    'ContextType': 'category',
+    }
+)
+
+## Functions for getting stats.
+
+def combine_dicts(list_of_dicts):
+    '''
+    Helper function to flatten a list of dicts.
+    (For some reason itertools.chain.from_iterable
+    was not working for this use case.)
+    '''
+    single_dict = {}
+    for d in list_of_dicts:
+       single_dict.update(d)
+    return single_dict
+
+def first_order_summary_stats(grp: pd.DataFrame) -> pd.Series:
+    '''
+    Calcuate summary stats for each first-order measure.
+    '''
+    cols = list(first_order_col_renames.values())
+    return pd.Series(combine_dicts(
+        [{
+        # Computations that don't require looping over cols.
+        'count' : grp[cols[0]].count()
+        }] + [
+        # list comprehension of dicts where
+        # each dict contains summary stats
+        # for one input column
+        {
+            col: sorted(list(grp[col])),
+            col + '_min': grp[col].min(),
+            col + '_q25': grp[col].quantile(0.25),
+            # col + '_mean': grp[col].mean(),
+            col + '_q50': grp[col].quantile(0.50),
+            col + '_q75': grp[col].quantile(0.75),
+            col + '_max': grp[col].max(),
+        } for col in cols
+    ]))
+
+# Group by `group_by_cols` (StimID, AnswerCertainty,
+# AnswerPolarity, ContextType). 
+d = (d
+    # Select columns of interest.
+    .loc[:,(group_by_cols + first_order_cols)]
+    # Rename first-order measures to shorter names.
+    .rename(columns=first_order_col_renames)
+    # Group by `group_by_cols` (StimID, AnswerCertainty,
+    # AnswerPolarity, ContextType).
+    .groupby(
+        group_by_cols, 
+        # No multi-indexing.
+        as_index=False,
+    )
+    # Get summary stats as columns.
+    .apply(first_order_summary_stats)
+    .dropna()
+)
+
+d.to_csv(data_by_item_path, index=False)
---
+++ qualitative-analysis/merge-stimuli.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+relevance_dir = Path(__file__).resolve().parent.parent
+# INPUT
+data_by_item_path = relevance_dir / 'qualitative-analysis' / 'data' / 'by_item.csv'
+relevance_stimuli_path = relevance_dir / 'trials' / 'relevance_stimuli.csv'
+# OUTPUT
+by_item_with_stimuli_path = relevance_dir / 'qualitative-analysis' / 'data' / 'by_item_with_stimuli.csv'
+
+# Add in stimuli
+
+st = pd.read_csv(relevance_stimuli_path)
+st = (st
+    .query('TaskType == "relevance"')
+)
+
+st['AnswerPolarity'] = np.where(
+    st['AnswerCertainty'] == 'non_answer',
+    'positive',
+    st['AnswerPolarity'])
+
+# Next step: merge in stimuli and see if the size of the df changes
+
+d = pd.read_csv(data_by_item_path)
+
+dst = d.merge(st,
+            how='left')
+
+
+drop_before_write = [
+    'TrialType',
+    'TaskType',    
+    'name',
+    'SliderLabelLeft',
+    'SliderLabelRight',
+    'TaskQuestion',
+]
+
+# write to csv
+
+(dst
+    .drop(drop_before_write, axis=1)
+    .to_csv(by_item_with_stimuli_path, index=False, 
+        float_format='%.2f'
+    )
+)
