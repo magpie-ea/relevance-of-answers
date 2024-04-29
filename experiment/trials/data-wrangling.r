eedbf1175cb82c883519c7ddd5b9181e494dd178
michael-franke <the.secret.goldfish@me.com>
2024-04-29 16:43:41 +0200

    separate 'experiment' folder

---
+++ experiment/trials/data-wrangling.r
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+stimuli <- read_csv('relevance_stimuli.csv') %>% 
+  arrange(StimID, AnswerCertainty, AnswerPolarity, ContextType)
+          
+          
+          
+          
+          
+          
+          
+          
+
