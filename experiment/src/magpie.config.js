d8d6f2f845ef81554c4a6a3572c62e2cc8ef30d0
michael-franke <the.secret.goldfish@me.com>
2025-03-12 11:30:26 +0100

    added completionURL

--- experiment/src/magpie.config.js
+++ experiment/src/magpie.config.js
@@ -3,7 +3,7 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   contactEmail: 'alexwarstadt@gmail.com',
-  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C5LQGSYV'
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C5LQGSYV',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'prolific'
 };
